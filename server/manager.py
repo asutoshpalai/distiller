1253dd07f4a9a93cdd8200c3c8422254ec6330ba
Asutosh Palai <asupalai@gmail.com>
2016-11-04 00:33:33 +0530

    Added docstrings for server classes

--- server/manager.py
+++ server/manager.py
@@ -1,3 +1,21 @@
+"""
+This class handles the clients, distribution of a task, serialization of the
+function and recombining of results.
+
+This class internally uses Connector class to manage connection with the
+clients. Due to blocking nature of the default Flask server, it spawns the
+server in a separate thread.
+
+For serialization of the functions, it uses cloudpickle and returns the pickle
+blob.
+
+For distribution of a given task, it partitions the given arguments into chunks
+based on the number of clients present. Then it assigns each chuck an id with
+which the client is expected to return the result.
+
+At present, it only supports map parallelisation and not reduce parallelism.
+"""
+
 from .connector import Connector
 from time import sleep
 import random, math
@@ -6,18 +24,25 @@ import cloudpickle
 
 class Manager:
     def __init__(self):
-        self.functions = {}
-        self.clients = []
-        self.computes = {}
-        self.id = 0
+        self.functions = {} # Function storage for serialisation
+        self.clients = [] # List of client's addresses
+        self.computes = {} # The map of id of chunks sent to the client for computation and their results
+        self.id = 0 # The last used up id
         self.connector = Connector(self)
         threading.Thread(target=self.connector.start).start()
 
     def register_client(self, client):
+        """When a new client connects, Connector calls this function to add the
+        client to internal list"""
         print("Received client {}".format(client))
         self.clients.append(client)
 
     def remote_exec(self, function, data):
+        """This function is to be called with a function and the data (a list)
+        to map using the function.
+        This distributes the task among the clients and then recombines the
+        result and returns the mapped values.
+        """
         size = len(data)
         n = len(self.clients)
 
@@ -40,7 +65,6 @@ class Manager:
                 sleep(1) # move to conditions
         res = []
         for k in range(lowest_id, highest_id + 1):
-            print(self.computes[k][2])
             res.extend(self.computes[k][2])
 
         return res
@@ -51,11 +75,15 @@ class Manager:
         self.connector.client_compute(id, client, fun_name, data)
 
     def get_function(self, fun_name):
+        """This serializes the function. When the client requests for a function
+        the connector calls this function to obtain it."""
         fun = self.functions[fun_name]
         blob = cloudpickle.dumps(fun)
         return blob
 
     def receive_result(self, id, data):
+        """When the client returns the result, connector calls this to store
+        the result"""
         self.computes[id][2] = data
 
     def failed(self, id):
--- server/parser.py
+++ server/parser.py
@@ -1,3 +1,11 @@
+"""
+This class handles parsing and execution of files.
+It evaluates the file in an environment with a special function `remote_exec`.
+
+`remote_exec` function is used by the programs to pass the function and the data
+that is to be executed on the client side and result is to be returned.
+"""
+
 from .manager import Manager
 
 class Parser():
@@ -7,6 +15,7 @@ class Parser():
         self.manager = manager
 
     def exec_file(self, file_path):
+        """This function can be called with a file to run it."""
         with open(file_path) as f:
             code = f.read()
 
