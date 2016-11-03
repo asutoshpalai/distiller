7fabe1e9d56532c81aba2e5c5dc09cca75b3ae47
Asutosh Palai <asupalai@gmail.com>
2016-11-03 23:19:07 +0530

    Updated GUI to show stdout

--- client_gui.py
+++ client_gui.py
@@ -2,27 +2,56 @@ from client.executor import Executor
 from tkinter import *
 from tkinter import Canvas, Entry, Label
 
-root = Tk()
-root.title('Distiller client')
+class Connector():
+    def __init__(self):
+        root = Tk()
+        root.title('Distiller client')
 
-ls = Label(text='Server address: ').pack(side=TOP, padx=10, pady=10)
-entrys = Entry(root, width=20)
-entrys.pack(side=TOP,padx=10,pady=10)
+        ls = Label(text='Server address: ').pack(side=TOP, padx=10, pady=10)
+        self.entrys = Entry(root, width=20)
+        self.entrys.pack(side=TOP,padx=10,pady=10)
 
-lc = Label(text='Local address: ').pack(side=TOP, padx=10, pady=10)
-entryc = Entry(root, width=20)
-entryc.pack(side=TOP,padx=10,pady=10)
+        lc = Label(text='Local address: ').pack(side=TOP, padx=10, pady=10)
+        self.entryc = Entry(root, width=20)
+        self.entryc.pack(side=TOP,padx=10,pady=10)
 
-lcp = Label(text='Port: ').pack(side=TOP, padx=10, pady=10)
-entrycp = Entry(root, width=20)
-entrycp.pack(side=TOP,padx=10,pady=10)
+        lcp = Label(text='Port: ').pack(side=TOP, padx=10, pady=10)
+        self.entrycp = Entry(root, width=20)
+        self.entrycp.pack(side=TOP,padx=10,pady=10)
 
+        Button(root, text='OK', command=self.onRegister).pack(side=LEFT)
+        self.root = root
 
-def onRegister():
-    serveraddr = entrys.get()
-    clientaddr = entryc.get()
-    clientport = entrycp.get()
-    Executor(serveraddr, clientport, clientaddr)
+    def start(self):
+        self.root.mainloop()
 
-Button(root, text='OK', command=onRegister).pack(side=LEFT)
-root.mainloop()
+    def onRegister(self):
+        serveraddr = self.entrys.get()
+        clientaddr = self.entryc.get()
+        clientport = self.entrycp.get()
+
+        self.root.destroy()
+
+        MainGUI(serveraddr, clientport, clientaddr).start()
+
+class MainGUI():
+    def __init__(self, serveraddr, clientport, clientaddr):
+        self.exe = Executor(serveraddr, clientport, clientaddr)
+
+        top = Tk()
+        text = Text(top)
+        text.pack()
+
+        import sys
+        sys.stdout = Std_redirector(text)
+        top.mainloop()
+
+class Std_redirector(object):
+    def __init__(self,widget):
+        self.widget = widget
+
+    def write(self,string):
+        self.widget.insert(END,string)
+        self.widget.see(END)
+
+Connector().start()
